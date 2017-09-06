4fad433d25cd49c3a355ffa96aac2e0a72d978c1
Alessandro Colleoni <alessandrocolleoni@Alessandros-MacBook-Pro.local>
2017-09-06 15:00:12 +0200

    feat(src index): export the components

---
+++ src/index.js
@@ -0,0 +1,20 @@
+import {
+  Fab,
+  InnerButton,
+  MainConfirmButton,
+  PopupButton,
+  PopupConfirmButton,
+  ToggleButton
+} from './components/buttons'
+import { Icon } from './components/Icon'
+import './style.css'
+
+export default {
+  Fab,
+  Icon,
+  InnerButton,
+  MainConfirmButton,
+  PopupButton,
+  PopupConfirmButton,
+  ToggleButton
+}
